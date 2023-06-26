1170f76ff5bc443546a77c6d3bebb29654103f23
88koala88 <kbch88@gmail.com>
2023-06-26 08:30:43 +0000

    func

--- func_list.py
+++ func_list.py
@@ -1,3 +1,8 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
 def daily_chart(stock_data):
     # plotly 시각화
     fig = make_subplots(specs=[[{"secondary_y": True}]])
