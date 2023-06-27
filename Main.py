8842455e61f4e94eb3f91a0e5940dccaeb3ea2d9
88koala88 <kbch88@gmail.com>
2023-06-27 09:00:06 +0000

    try

--- Main.py
+++ Main.py
@@ -49,20 +49,17 @@ sector_nm = 'Information Technology'
 
 df = df[df['sector'] == sector_nm]
 
-def treemap(df)
 
-    fig = px.treemap(df, path=[px.Constant(sector_nm), 'industry', 'corp_name'], values='Volume',
+fig = px.treemap(df, path=[px.Constant(sector_nm), 'industry', 'corp_name'], values='Volume',
                   color='Volume', hover_data=['Volume'],
                   color_continuous_scale='RdBu',
                   color_continuous_midpoint=np.average(df['Volume'], weights=df['Volume']))
 
-    fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
+fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
 
-    return(fig)
+    
 
-
-
-st.plotly_chart(treemap, use_container_width=True)
+st.plotly_chart(fig, use_container_width=True)
 
 
 
