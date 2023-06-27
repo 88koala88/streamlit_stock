c10ea7943047b4d3e356421e6b78415143b59e68
88koala88 <kbch88@gmail.com>
2023-06-27 09:43:29 +0000

    try

--- Main.py
+++ Main.py
@@ -63,5 +63,11 @@ df1 = df[df['sector'] == sector_nm]
 
 
 
-
-
+df = px.data.gapminder().query("year == 2007")
+fig = px.treemap(df, path=[px.Constant("world"), 'continent', 'country'], values='pop',
+                  color='lifeExp', hover_data=['iso_alpha'],
+                  color_continuous_scale='RdBu',
+                  color_continuous_midpoint=np.average(df['lifeExp'], weights=df['pop']))
+fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
+# fig.show()
+st.plotly_chart(fig, use_container_width=True)
