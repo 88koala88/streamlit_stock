522d5d9dc056441b14652a66614f8b26eb90c769
88koala88 <kbch88@gmail.com>
2023-07-02 02:17:14 +0900

    a

--- Main.py
+++ Main.py
@@ -59,13 +59,13 @@ kosdaq= conn.read("data1-study1/kosdaq_index.csv", input_format="csv", ttl=600)
 
 
 df1 = kospi
-fig1 = px.line(df1, title = '코스피 지수')
+fig1 = px.line(df1, x='날짜', y='종가', title = '코스피 지수')
 st.plotly_chart(fig1, use_container_width=True)
 
     
 
 df2 = kosdaq
-fig2 = px.line(df2, title = '코스닥 지수')
+fig2 = px.line(df2, x='날짜', y='종가', title = '코스닥 지수')
 st.plotly_chart(fig2, use_container_width=True)
 
 
