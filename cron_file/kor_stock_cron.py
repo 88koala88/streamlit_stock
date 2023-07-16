e626fa16a95f1ec0797c033b4e7426de962f7a98
88koala88 <kbch88@gmail.com>
2023-07-16 17:26:06 +0900

    a

--- cron_file/kor_stock_cron.py
+++ cron_file/kor_stock_cron.py
@@ -149,78 +149,78 @@ else:
 
 # In[ ]:
 
-print(f'{file_name} end')
+# print(f'{file_name} end')
     
-# ###   DIV/BPS/PER/EPS 조회 (매일 실행 되는 배치용)
+# # ###   DIV/BPS/PER/EPS 조회 (매일 실행 되는 배치용)
 
-file_name = 'kor_stock_trading_value_by_investor'
-print(f'{file_name} start')
+# file_name = 'kor_stock_trading_value_by_investor'
+# print(f'{file_name} start')
 
 
-print('거래실적 (거래대금)수집 시작')
-# In[34]:
-ticker_nm = '005930'
-# time.sleep(300)
-buy_sell_type_list = ['순매수', '매수', '매도']
-for buy_sell_type in buy_sell_type_list:
-    for ticker_nm in kor_ticker_list:
-        file_name = 'kor_stock_trading_value_by_investor'
-        try:
-            kor_stock_trading_value_by_investor = stock.get_market_trading_value_by_date(today_date1, today_date1, 
-                                                                             ticker_nm, 
-                                                                             detail=True,
-                                                                             on = buy_sell_type)
-            kor_stock_trading_value_by_investor = kor_stock_trading_value_by_investor.reset_index()
-            kor_stock_trading_value_by_investor['ticker'] = ticker_nm
-            kor_stock_trading_value_by_investor['type'] = buy_sell_type
+# print('거래실적 (거래대금)수집 시작')
+# # In[34]:
+# ticker_nm = '005930'
+# # time.sleep(300)
+# buy_sell_type_list = ['순매수', '매수', '매도']
+# for buy_sell_type in buy_sell_type_list:
+#     for ticker_nm in kor_ticker_list:
+#         file_name = 'kor_stock_trading_value_by_investor'
+#         try:
+#             kor_stock_trading_value_by_investor = stock.get_market_trading_value_by_date(today_date1, today_date1, 
+#                                                                              ticker_nm, 
+#                                                                              detail=True,
+#                                                                              on = buy_sell_type)
+#             kor_stock_trading_value_by_investor = kor_stock_trading_value_by_investor.reset_index()
+#             kor_stock_trading_value_by_investor['ticker'] = ticker_nm
+#             kor_stock_trading_value_by_investor['type'] = buy_sell_type
             
-            df = kor_stock_trading_value_by_investor
+#             df = kor_stock_trading_value_by_investor
             
-            if not os.path.exists(f'/home/kbch88/streamlit_stock/data_crawler/{file_name}_{today_date1}.csv'):
-                df.to_csv(f'/home/kbch88/streamlit_stock/data_crawler/{file_name}_{today_date1}.csv', index=False, mode='w')
-            else:
-                df.to_csv(f'/home/kbch88/streamlit_stock/data_crawler/{file_name}_{today_date1}.csv', index=False, mode='a', header=False)
+#             if not os.path.exists(f'/home/kbch88/streamlit_stock/data_crawler/{file_name}_{today_date1}.csv'):
+#                 df.to_csv(f'/home/kbch88/streamlit_stock/data_crawler/{file_name}_{today_date1}.csv', index=False, mode='w')
+#             else:
+#                 df.to_csv(f'/home/kbch88/streamlit_stock/data_crawler/{file_name}_{today_date1}.csv', index=False, mode='a', header=False)
             
-            log_df(file_name, f'success_{buy_sell_type}')    
-        except:
-            log_df(file_name, f'fail_{buy_sell_type}')
-    #time.sleep(300)
+#             log_df(file_name, f'success_{buy_sell_type}')    
+#         except:
+#             log_df(file_name, f'fail_{buy_sell_type}')
+#     #time.sleep(300)
         
-print('거래실적 (거래대금)수집 완료')        
+# print('거래실적 (거래대금)수집 완료')        
 
 
-# ### 일자별 거래실적 추이 (거래대금) 배치용
+# # ### 일자별 거래실적 추이 (거래대금) 배치용
 
-# In[ ]:
+# # In[ ]:
 
 
-print('거래실적 (거래량)수집 시작')     
-# In[38]:
-for buy_sell_type in buy_sell_type_list:
-    for ticker_nm in kor_ticker_list:
-        file_name = 'kor_stock_trading_volume_by_date'
-        try:
-            kor_stock_trading_volume_by_date = stock.get_market_trading_volume_by_date(today_date1, today_date1, 
-                                                                             ticker_nm, 
-                                                                             detail=True,
-                                                                             on =buy_sell_type)
-            kor_stock_trading_volume_by_date = kor_stock_trading_volume_by_date.reset_index()
-            kor_stock_trading_volume_by_date['ticker'] = ticker_nm
-            kor_stock_trading_volume_by_date['type'] = buy_sell_type
+# print('거래실적 (거래량)수집 시작')     
+# # In[38]:
+# for buy_sell_type in buy_sell_type_list:
+#     for ticker_nm in kor_ticker_list:
+#         file_name = 'kor_stock_trading_volume_by_date'
+#         try:
+#             kor_stock_trading_volume_by_date = stock.get_market_trading_volume_by_date(today_date1, today_date1, 
+#                                                                              ticker_nm, 
+#                                                                              detail=True,
+#                                                                              on =buy_sell_type)
+#             kor_stock_trading_volume_by_date = kor_stock_trading_volume_by_date.reset_index()
+#             kor_stock_trading_volume_by_date['ticker'] = ticker_nm
+#             kor_stock_trading_volume_by_date['type'] = buy_sell_type
             
-            df = kor_stock_trading_volume_by_date
+#             df = kor_stock_trading_volume_by_date
             
-            if not os.path.exists(f'/home/kbch88/streamlit_stock/data_crawler/{file_name}_{today_date1}.csv'):
-                df.to_csv(f'/home/kbch88/streamlit_stock/data_crawler/{file_name}_{today_date1}.csv', index=False, mode='w')
-            else:
-                df.to_csv(f'/home/kbch88/streamlit_stock/data_crawler/{file_name}_{today_date1}.csv', index=False, mode='a', header=False)
+#             if not os.path.exists(f'/home/kbch88/streamlit_stock/data_crawler/{file_name}_{today_date1}.csv'):
+#                 df.to_csv(f'/home/kbch88/streamlit_stock/data_crawler/{file_name}_{today_date1}.csv', index=False, mode='w')
+#             else:
+#                 df.to_csv(f'/home/kbch88/streamlit_stock/data_crawler/{file_name}_{today_date1}.csv', index=False, mode='a', header=False)
             
-            log_df(file_name, 'success')    
-        except:
-            log_df(file_name, 'fail')
-    #time.sleep(300)
+#             log_df(file_name, 'success')    
+#         except:
+#             log_df(file_name, 'fail')
+#     #time.sleep(300)
     
-print('거래실적 (거래량)수집 완료')         
+# print('거래실적 (거래량)수집 완료')         
 
 
 # In[ ]:
